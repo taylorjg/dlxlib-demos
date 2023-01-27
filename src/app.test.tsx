c0063fdbba201485aca0bbb552799235b09bb800
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2023-01-27 15:56:55 +0000

    More deployment prep

--- src/app.test.tsx
+++ src/app.test.tsx
@@ -3,5 +3,5 @@ import { App } from "./app"
 
 test("renders home page", () => {
   render(<App />)
-  expect(screen.getByText("Demo 1")).toBeInTheDocument()
+  expect(screen.getByText("N Queens")).toBeInTheDocument()
 })
