e316428c610f043322387cb57627380c185ca9cf
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2023-02-01 10:00:18 +0000

    Add hardcoded solutions to thumbnails for Sudoku and N Queens

--- src/demos/n-queens/thumbnail.tsx
+++ src/demos/n-queens/thumbnail.tsx
@@ -1,8 +1,29 @@
 import { last } from "utils"
 import { NQueensDrawing } from "./drawing"
 import { puzzles } from "./puzzles"
+import { Puzzle } from "./puzzle"
+import { NQueensInternalRow } from "./internal-row"
 
 export const NQueensThumbnail = () => {
   const puzzle = last(puzzles)
-  return <NQueensDrawing puzzle={puzzle} solutionInternalRows={[]} />
+  const solutionInternalRows = makeSolution(puzzle)
+  return <NQueensDrawing puzzle={puzzle} solutionInternalRows={solutionInternalRows} />
+}
+
+const makeSolution = (puzzle: Puzzle): NQueensInternalRow[] => {
+  return [
+    makeInternalRow(puzzle, 0, 0),
+    makeInternalRow(puzzle, 1, 4),
+    makeInternalRow(puzzle, 2, 7),
+    makeInternalRow(puzzle, 3, 5),
+    makeInternalRow(puzzle, 4, 2),
+    makeInternalRow(puzzle, 5, 6),
+    makeInternalRow(puzzle, 6, 1),
+    makeInternalRow(puzzle, 7, 3)
+  ]
+}
+
+const makeInternalRow = (puzzle: Puzzle, row: number, col: number): NQueensInternalRow => {
+  const coords = { row, col }
+  return { puzzle, coords }
 }
--- src/demos/sudoku/thumbnail.tsx
+++ src/demos/sudoku/thumbnail.tsx
@@ -1,8 +1,40 @@
-import { first } from "utils"
+import { first, range, sameCoords } from "utils"
 import { SudokuDrawing } from "./drawing"
+import { SudokuInternalRow } from "./internal-row"
+import { Puzzle } from "./puzzle"
 import { puzzles } from "./puzzles"
 
 export const SudokuThumbnail = () => {
   const puzzle = first(puzzles)
-  return <SudokuDrawing puzzle={puzzle} solutionInternalRows={[]} />
+  const solution = [
+    "614892753",
+    "893517264",
+    "257346918",
+    "432185679",
+    "569274381",
+    "781639542",
+    "375428196",
+    "146953827",
+    "928761435"
+  ]
+  const solutionInternalRows = parseSolution(puzzle, solution)
+  return <SudokuDrawing puzzle={puzzle} solutionInternalRows={solutionInternalRows} />
+}
+
+const parseSolution = (puzzle: Puzzle, solution: string[]): SudokuInternalRow[] => {
+  const internalRows: SudokuInternalRow[] = []
+  const size = solution.length
+  for (const row of range(size)) {
+    for (const col of range(size)) {
+      const ch = solution[row][col]
+      const value = Number(ch)
+      if (Number.isInteger(value)) {
+        const coords = { row, col }
+        const isInitialValue = puzzle.initialValues.findIndex(initialValue => sameCoords(initialValue.coords, coords)) >= 0
+        const internalRow = {coords, value, isInitialValue}
+        internalRows.push(internalRow)
+      }
+    }
+  }
+  return internalRows
 }
