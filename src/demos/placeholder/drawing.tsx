fa50109a3345473d95953987877ce8c3dce05a70
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2023-02-03 04:31:08 +0000

    Extract draw functions in placeholder drawing

--- src/demos/placeholder/drawing.tsx
+++ src/demos/placeholder/drawing.tsx
@@ -4,13 +4,21 @@ const VIEWBOX_WIDTH = 100
 const VIEWBOX_HEIGHT = 100
 
 export const PlaceholderDrawing: React.FC<DrawingProps<{}, {}>> = () => {
-  const cx = VIEWBOX_WIDTH / 2
-  const cy = VIEWBOX_HEIGHT / 2
+
+  const drawBackground = (): JSX.Element => {
+    return <rect x={0} y={0} width={VIEWBOX_WIDTH} height={VIEWBOX_HEIGHT} fill="white" />
+  }
+
+  const drawMessage = (): JSX.Element => {
+    const cx = VIEWBOX_WIDTH / 2
+    const cy = VIEWBOX_HEIGHT / 2
+    return <text x={cx} y={cy} textAnchor="middle" dominantBaseline="middle">TODO</text>
+  }
 
   return (
     <svg viewBox={`0 0 ${VIEWBOX_WIDTH} ${VIEWBOX_HEIGHT}`}>
-      <rect x={0} y={0} width={VIEWBOX_WIDTH} height={VIEWBOX_HEIGHT} fill="white" />
-      <text x={cx} y={cy} textAnchor="middle" dominantBaseline="middle">TODO</text>
+      {drawBackground()}
+      {drawMessage()}
     </svg>
   )
 }
