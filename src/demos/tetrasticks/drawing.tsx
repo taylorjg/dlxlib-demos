e3e65de894e35d57bff93f954b022704cc1cbffd
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2023-02-07 23:20:17 +0000

    Start to improve drawing of tetrasticks

--- src/demos/tetrasticks/drawing.tsx
+++ src/demos/tetrasticks/drawing.tsx
@@ -16,6 +16,9 @@ const SQUARE_HEIGHT = (VIEWBOX_HEIGHT - TETRASTICK_FULL_THICKNESS) / 5
 const calculateX = (col: number) => col * SQUARE_WIDTH + TETRASTICK_HALF_THICKNESS
 const calculateY = (row: number) => row * SQUARE_HEIGHT + TETRASTICK_HALF_THICKNESS
 
+const calculatePoint = (location: Coords, coords: Coords) =>
+  [calculateX(location.col + coords.col), calculateY(location.row + coords.row)]
+
 const tetraStickColours = new Map<string, string>([
   ["F", "#FF7366"],
   ["H", "#00E61A"],
@@ -98,55 +101,90 @@ export const Drawing: React.FC<DrawingProps<Puzzle, InternalRow>> = ({
   // }
 
   const drawTetraSticks = (): JSX.Element[] => {
-    return solutionInternalRows.flatMap(drawTetraStick)
+    return solutionInternalRows.flatMap(drawPiece)
   }
 
-  const drawTetraStick = (internalRow: InternalRow): JSX.Element[] => {
-    const hs = internalRow.variation.horizontals.map(h => drawHorizontalLineSegment(internalRow, h))
-    const vs = internalRow.variation.verticals.map(v => drawVerticalLineSegment(internalRow, v))
-    return hs.concat(vs)
+  const LINE_END_MULTIPLIER = 3
+  const ROUNDED_CORNER_MULTIPLIER = 4
+
+  const insetPoint = (multiplier: number) => (coords1: Coords, coords2: Coords, point: number[]): number[] => {
+    const rowDiff = coords2.row - coords1.row
+    const colDiff = coords2.col - coords1.col
+    const verticalInset = TETRASTICK_HALF_THICKNESS * multiplier
+    const horizontalInset = TETRASTICK_HALF_THICKNESS * multiplier
+    const [x, y] = point
+    if (rowDiff === 1 && colDiff === 0) return [x, y + verticalInset]
+    if (rowDiff === -1 && colDiff === 0) return [x, y - verticalInset]
+    if (rowDiff === 0 && colDiff === 1) return [x + horizontalInset, y]
+    if (rowDiff === 0 && colDiff === -1) return [x - horizontalInset, y]
+    return point
   }
 
-  const drawHorizontalLineSegment = (internalRow: InternalRow, coords: Coords): JSX.Element => {
-    const actualRow = internalRow.location.row + coords.row
-    const actualCol = internalRow.location.col + coords.col
-    const x1 = calculateX(actualCol)
-    const x2 = calculateX(actualCol + 1)
-    const y = calculateY(actualRow)
-    const stroke = tetraStickColours.get(internalRow.label)
+  const insetLineStart = insetPoint(LINE_END_MULTIPLIER)
+  const insetLineEnd = insetPoint(LINE_END_MULTIPLIER)
+  const insetRoundedCornerStart = insetPoint(ROUNDED_CORNER_MULTIPLIER)
+  const insetRoundedCornerEnd = insetPoint(ROUNDED_CORNER_MULTIPLIER)
 
-    return (
-      <line
-        key={`hline-${actualRow}-${actualCol}`}
-        x1={x1}
-        y1={y}
-        x2={x2}
-        y2={y}
-        stroke={stroke}
-        strokeWidth={TETRASTICK_FULL_THICKNESS}
-      />
-    )
+  // https://stackoverflow.com/a/40444735
+  // const angle = ([a,b],[c,d],[e,f]) => (Math.atan2(f-d,e-c)-Math.atan2(b-d,a-c)+3*pi)%(2*pi)-pi;
+  // const sweepFl = (S,V,E) => angle(E,S,V) > 0 ? 0 : 1;
+  const angle = (S: Coords, V: Coords, E: Coords) => {
+    const { row: b, col: a } = E
+    const { row: d, col: c } = S
+    const { row: f, col: e } = V
+    return (Math.atan2(f - d, e - c) - Math.atan2(b - d, a - c) + 3 * Math.PI) % (2 * Math.PI) - Math.PI
   }
 
-  const drawVerticalLineSegment = (internalRow: InternalRow, coords: Coords): JSX.Element => {
-    const actualRow = internalRow.location.row + coords.row
-    const actualCol = internalRow.location.col + coords.col
-    const x = calculateX(actualCol)
-    const y1 = calculateY(actualRow)
-    const y2 = calculateY(actualRow + 1)
-    const stroke = tetraStickColours.get(internalRow.label)
+  const moveTo = (point: number[]): string => {
+    const [x, y] = point
+    return `M ${x},${y}`
+  }
 
-    return (
-      <line
-        key={`vline-${actualRow}-${actualCol}`}
-        x1={x}
-        y1={y1}
-        x2={x}
-        y2={y2}
-        stroke={stroke}
-        strokeWidth={TETRASTICK_FULL_THICKNESS}
-      />
-    )
+  const lineTo = (point: number[]): string => {
+    const [x, y] = point
+    return `L ${x},${y}`
+  }
+
+  const roundedCornerTo = (S: Coords, V: Coords, E: Coords, point: number[]): string => {
+    const r = TETRASTICK_HALF_THICKNESS * ROUNDED_CORNER_MULTIPLIER
+    const largeArcFlag = 0
+    const sweepFlag = angle(S, V, E) < 0 ? 1 : 0
+    const [x, y] = point
+    return `A ${r} ${r} 0 ${largeArcFlag} ${sweepFlag} ${x},${y}`
+  }
+
+  const drawPiece = (internalRow: InternalRow): JSX.Element[] => {
+
+    const { label, variation, location } = internalRow
+    const colour = tetraStickColours.get(label)
+    const pathData: string[] = []
+
+    for (const polyLine of variation.polyLines) {
+      const pathCommands: string[] = []
+      pathCommands.push(moveTo(calculatePoint(location, polyLine[0])))
+      for (const coords of polyLine.slice(1)) {
+        pathCommands.push(lineTo(calculatePoint(location, coords)))
+      }
+      pathData.push(pathCommands.join(" "))
+    }
+
+    const elements: JSX.Element[] = []
+
+    pathData.forEach((d, index) => {
+      const path = (
+        <path
+          key={`tetrastick-${label}-${index}`}
+          d={d}
+          stroke={colour}
+          strokeWidth={TETRASTICK_FULL_THICKNESS}
+          strokeLinecap="round"
+          fill="none"
+        />
+      )
+      elements.push(path)
+    })
+
+    return elements
   }
 
   return (
