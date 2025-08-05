09b922e01ec733c98fb497fff721e1a1fb5be332
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2025-08-05 22:55:56 +0100

    Improve aristotle-numbers tests

--- src/demos/aristotle-numbers/aristotle-numbers.test.ts
+++ src/demos/aristotle-numbers/aristotle-numbers.test.ts
@@ -1,5 +1,5 @@
 import { findFirstSolution } from "test-helpers";
-import { sum } from "utils";
+import { range, sum } from "utils";
 import { Demo, horizontalRuns, diagonal1Runs, diagonal2Runs } from "./demo";
 import { InternalRow } from "./internal-row";
 import { makeThumbnailSolution } from "./thumbnail";
@@ -37,13 +37,32 @@ const checkDigits = (internalRows: InternalRow[]): void => {
   }
 };
 
+const makeMap = (internalRows: InternalRow[]): Map<number, number> => {
+  const map = new Map<number, number>();
+
+  for (const internalRow of internalRows) {
+    for (const index of range(internalRow.run.cellIds.length)) {
+      const cellId = internalRow.run.cellIds[index];
+      const value = internalRow.values[index];
+      console.assert(!map.has(cellId));
+      map.set(cellId, value);
+    }
+  }
+
+  return map;
+};
+
 const checkRuns = (internalRows: InternalRow[]): void => {
   const getInternalRowsOfRunType = (runType: RunType): InternalRow[] =>
     internalRows.filter((internalRow) => internalRow.run.runType === runType);
 
-  expect(getInternalRowsOfRunType(RunType.Horizontal)).toHaveLength(5);
-  expect(getInternalRowsOfRunType(RunType.Diagonal1)).toHaveLength(5);
-  expect(getInternalRowsOfRunType(RunType.Diagonal2)).toHaveLength(5);
+  const horizontalRows = getInternalRowsOfRunType(RunType.Horizontal);
+  const diagonal1Rows = getInternalRowsOfRunType(RunType.Diagonal1);
+  const diagonal2Rows = getInternalRowsOfRunType(RunType.Diagonal2);
+
+  expect(horizontalRows).toHaveLength(5);
+  expect(diagonal1Rows).toHaveLength(5);
+  expect(diagonal2Rows).toHaveLength(5);
 
   const internalRowRuns = internalRows.map(({ run }) => run);
 
@@ -59,18 +78,42 @@ const checkRuns = (internalRows: InternalRow[]): void => {
     expect(internalRowRuns).toContain(run);
   }
 
-  const values = internalRows.flatMap((internalRow) => internalRow.values);
-  const valuesAsASet = new Set(values);
-  expect(valuesAsASet.size).toBe(19);
+  checkValues(horizontalRows);
+  checkValues(diagonal1Rows);
+  checkValues(diagonal2Rows);
 
-  // TODO: check that the values are in the same locations for horizontal, diagonal1 and diagonal2 solution rows
-  // horizontal: cellId 0 = v1, cellId 1 = v2, cellId 2 = v3, etc.
-  // diagonal1: cellId 0 = v1, cellId 1 = v2, cellId 2 = v3, etc.
-  // diagonal2: cellId 0 = v1, cellId 1 = v2, cellId 2 = v3, etc.
+  const horizontalMap = makeMap(horizontalRows);
+  const diagonal1Map = makeMap(diagonal1Rows);
+  const diagonal2Map = makeMap(diagonal2Rows);
+
+  for (const cellId of range(19)) {
+    expect(horizontalMap.has(cellId)).toBe(true);
+    expect(diagonal1Map.has(cellId)).toBe(true);
+    expect(diagonal2Map.has(cellId)).toBe(true);
+
+    const horizontalValue = horizontalMap.get(cellId);
+    const diagonal1Value = diagonal1Map.get(cellId);
+    const diagonal2Value = diagonal2Map.get(cellId);
+
+    expect(diagonal1Value).toBe(horizontalValue);
+    expect(diagonal2Value).toBe(horizontalValue);
+  }
 };
 
 const checkRunSums = (internalRows: InternalRow[]): void => {
   for (const internalRow of internalRows) {
     expect(sum(internalRow.values)).toBe(38);
+  }
+};
+
+const checkValues = (internalRows: InternalRow[]): void => {
+  const values = internalRows.flatMap((internalRow) => internalRow.values);
+  const setOfValues = new Set(values);
+
+  expect(setOfValues.size).toBe(19);
+
+  for (const index of range(19)) {
+    const value = index + 1;
+    expect(setOfValues).toContain(value);
   }
 };
